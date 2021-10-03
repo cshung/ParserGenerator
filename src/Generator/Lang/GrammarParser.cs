68852d14848c198fd3e9cc6236d0108c2fda4444
Andrew Au <cshung@gmail.com>
2021-10-03 10:45:23 -0700

    Remove imports

--- src/Generator/Lang/GrammarParser.cs
+++ src/Generator/Lang/GrammarParser.cs
@@ -3,9 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    
+
     internal class GrammarParser
     {
         private LexicalAnalyzer lexer;
--- src/Generator/Lang/RegularExpressionParser.cs
+++ src/Generator/Lang/RegularExpressionParser.cs
@@ -1,10 +1,7 @@
 ﻿namespace Andrew.ParserGenerator
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class RegularExpressionParser
     {
